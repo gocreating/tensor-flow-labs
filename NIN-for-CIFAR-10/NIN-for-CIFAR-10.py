5ec4d7dc35f02deee85aca3f917dccdd80388098
翁治平 <gocreating@gmail.com>
2017-03-20 16:04:41 +0800

    Use random bias

--- NIN-for-CIFAR-10/NIN-for-CIFAR-10.py
+++ NIN-for-CIFAR-10/NIN-for-CIFAR-10.py
@@ -105,8 +105,8 @@ if __name__ == '__main__':
     x_image = tf.reshape(x, [-1, 32, 32, 3])
 
     ## conv-1
-    W_conv1 = weight_variable([5, 5, 3, 192]) # patch 5x5, in size 1, out size 32
-    b_conv1 = bias_variable([192])
+    W_conv1 = weight_variable([5, 5, 3, 192])
+    b_conv1 = tf.Variable(tf.random_normal([192], stddev=0.01, dtype=tf.float32))
     output = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 
     ## mlp-1-1
@@ -127,7 +127,7 @@ if __name__ == '__main__':
 
     ## conv-2
     W_conv2 = weight_variable([5, 5, 96, 192])
-    b_conv2 = bias_variable([192])
+    b_conv2 = tf.Variable(tf.random_normal([192], stddev=0.01, dtype=tf.float32))
     output = tf.nn.relu(conv2d(output, W_conv2) + b_conv2)
 
     ## mlp-2-1
@@ -148,7 +148,7 @@ if __name__ == '__main__':
 
     ## conv-3 layer
     W_conv3 = weight_variable([3, 3, 192, 192])
-    b_conv3 = bias_variable([192])
+    b_conv3 = tf.Variable(tf.random_normal([192], stddev=0.01, dtype=tf.float32))
     output = tf.nn.relu(conv2d(output, W_conv3) + b_conv3)
 
     ## mlp-2-1
